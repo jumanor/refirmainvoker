aaffec40e6e23b2f7c14fc9dd011ad0fb55ea0ed
jumanorwin10 <jumanor@gmail.com>
2023-01-17 20:36:42 -0500

    actualizamos ejemplo03 reconocimiento grafico de firmas

--- example/lib/firmaPosicion.js
+++ example/lib/firmaPosicion.js
@@ -15,6 +15,9 @@ export class FirmaPosicion{
     COLOR;
     SCALE=1;
 
+    SIGNATURES=[];
+    IMAGEN_INICIAL_SOBRE_SIGNATURE=false;
+
      ///////////////////////////////////////PUBLIC///////////////////////////////////////////////////////////
     constructor(canvas){
         
@@ -47,6 +50,11 @@ export class FirmaPosicion{
         x=x*this.SCALE
         y=y*this.SCALE
 
+        if(this.restrictSignature(x,y,this.getWithRectangleScale(),this.getHeightRectangleScale())){
+            //Por ahora una excepcion en caso la imagen inicial este sobre un signature
+            this.IMAGEN_INICIAL_SOBRE_SIGNATURE=true;
+        }
+
         this.LAST_RECTANGLE_X=x;
         this.LAST_RECTANGLE_Y=y;
         this.COLOR=color;
@@ -60,22 +68,44 @@ export class FirmaPosicion{
 
         return this.LAST_RECTANGLE_Y/this.SCALE;
     }
+    dibujarSignatures(canvasContentPDF,signatures,color){
+
+        signatures.forEach(signRect =>{
+
+               //  Rect [10.0 718.5 178.0 780.0]
+               
+               let alto=(signRect[3]-signRect[1])*this.SCALE
+               let ancho=(signRect[2]-signRect[0])*this.SCALE
+               let x=signRect[0]*this.SCALE
+               let y=(this.CANVAS.height-signRect[1]*this.SCALE)-alto
+               
+               this.dibujarRect(canvasContentPDF,x,y,ancho,alto,color) 
+               
+               this.SIGNATURES.push([x,y,ancho,alto])//valors escalados
+        })
+    }
     ///////////////////////////////////////PRIVATE///////////////////////////////////////////////////////////
     getWithRectangleScale(){
 
         var width=this.WIDTH_RECTANGLE*this.SCALE
-        if (width % 1 !== 0)
-            throw new Error("El ancho del rectangulo tiene que ser entero")
+        //if (width % 1 !== 0)
+        //    throw new Error("El ancho del rectangulo tiene que ser entero")
 
         return width
     }
     getHeightRectangleScale(){
 
         var height=this.HEIGHT_RECTANGLE*this.SCALE
-        if (height % 1 !== 0)
-            throw new Error("El ancho del rectangulo tiene que ser entero")
+        //if (height % 1 !== 0)
+        //    throw new Error("El ancho del rectangulo tiene que ser entero")
                 
         return height
+    }
+    dibujarRect(canvas,x,y,ancho,alto,color){
+        
+        var ctx = canvas.getContext("2d");
+        ctx.fillStyle = color
+        ctx.fillRect(x, y, ancho, alto);
     }
     dibujar(x,y,color){
         var ctx = this.CANVAS.getContext("2d");
@@ -84,7 +114,50 @@ export class FirmaPosicion{
     }
     clear(x,y){
         var ctx = this.CANVAS.getContext("2d");
-        ctx.clearRect(x,y, this.getWithRectangleScale(), this.getHeightRectangleScale())
+        //un rectangulo mas grande por el redondeo a entero en caso se escale
+        ctx.clearRect(x-2,y-2, this.getWithRectangleScale()+4, this.getHeightRectangleScale()+4)
+    }
+    restrictSignature(xR,yR,anchoR,altoR){
+
+        for (let i=0;i<this.SIGNATURES.length;i++){
+
+            let signRect=this.SIGNATURES[i];
+
+            let alto=signRect[3]
+            let ancho=signRect[2]
+            let x=signRect[0]
+            let y=signRect[1]
+
+            if( xR<=(x+ancho)  &&  xR>=x  &&  yR<=(y+alto) && yR>=y){
+                return true;
+            }
+            if( (xR+anchoR)<=(x+ancho)  &&  (xR+anchoR)>=x  &&  yR<=(y+alto) && yR>=y){
+                return true;
+            }
+            if( (xR+anchoR)<=(x+ancho)  &&  (xR+anchoR)>=x  &&  (yR+altoR)<=(y+alto) && (yR+altoR)>=y){
+                return true;
+            }
+            if( xR<=(x+ancho)  &&  xR>=x  &&  (yR+altoR)<=(y+alto) && (yR+altoR)>=y){
+                return true;
+            }
+
+
+            if( x<=(xR+anchoR)  &&  x>=xR  &&  y<=(yR+altoR) && y>=yR){
+                return true;
+            }
+            if( (x+ancho)<=(xR+anchoR)  &&  (x+ancho)>=xR  &&  y<=(yR+altoR) && y>=yR){
+                return true;
+            }
+            if( (x+ancho)<=(xR+anchoR)  &&  (x+ancho)>=xR  &&  (y+alto)<=(yR+altoR) && (y+alto)>=yR){
+                return true;
+            }
+            if( x<=(xR+anchoR)  &&  x>=xR  &&  (y+alto)<=(yR+altoR) && (y+alto)>=yR){
+                return true;
+            }
+
+        };
+
+        return false;
     }
     mouseover(e){
         this.CANVAS.style.cursor="grab"  
@@ -122,6 +195,17 @@ export class FirmaPosicion{
         var dx = e.offsetX - this.LAST_POINTER_X;
         var dy = e.offsetY - this.LAST_POINTER_Y;
 
+        if(this.restrictSignature(this.LAST_RECTANGLE_X+dx, this.LAST_RECTANGLE_Y+dy,this.getWithRectangleScale(),this.getHeightRectangleScale())){
+            if(this.IMAGEN_INICIAL_SOBRE_SIGNATURE===false){
+                return
+            }
+        }
+        else{//Sin restricciones
+            if(this.IMAGEN_INICIAL_SOBRE_SIGNATURE===true){//Solo si la imagen de inicio esta restriginda
+                this.IMAGEN_INICIAL_SOBRE_SIGNATURE=false
+            }
+        }
+            
         
         var _canvas=this.CANVAS; 
        
---
+++ example/lib/signature.js
@@ -0,0 +1,91 @@
+export class Signature{
+    //////////////////////////////////////////////////////////////////////PUBLIC///////////////////////////////////////////////////////////
+    signaturesByPage(pdfTexto,pagNumber){
+        
+        if(this._formatoCorrecto(pdfTexto)===false){
+            return [];
+        }
+      
+        if(pdfTexto.includes('/T (Signature')){ 
+
+            let mapSignatures = new Map()
+
+            let arrayDeCadenas = pdfTexto.split('/T (Signature');
+            arrayDeCadenas.shift();                    
+            arrayDeCadenas.forEach(element => {
+                
+                let indexIni=element.indexOf("/P")
+                let indexFin=element.indexOf("0 R",indexIni)
+
+                let pageObj=element.slice(indexIni,indexFin).split(" ")[1]
+                let rect = element.slice(element.indexOf("[")+1,element.indexOf("]")).split(" ");
+
+                if(rect[3]==rect[4] && rect[3]<=1){//firma sin representacion grafica [0.0 0.0 0.0 0.0]
+                    return;
+                }
+                
+                if(mapSignatures.size==0){
+                    mapSignatures.set(pageObj,[rect])
+                }
+                else{
+                    let arr=mapSignatures.get(pageObj)
+                    if(arr==undefined){
+                        mapSignatures.set(pageObj,[rect])
+                    }
+                    else{
+                        arr.push(rect)
+                    }
+                }
+
+            });
+
+            //console.log(mapSignatures)
+
+            let arregloDeSignatures=[];
+            let pag=this._getPagesObj(pdfTexto);//los page object ya estan ordenados
+            for (let i=0;i<pag.length;i++){
+                if(pagNumber==i+1){
+
+                    let pageObj=pag[i];
+
+                    let map=mapSignatures.get(pageObj);
+                    if(map==undefined){
+
+                        arregloDeSignatures=[];
+                    }
+                    else{
+
+                        arregloDeSignatures=map;
+                    }
+
+                }
+            }
+            //console.log(arregloDeSignatures)
+            return arregloDeSignatures
+        }
+    }
+    //////////////////////////////////////////////////////////////////////PRIVATE///////////////////////////////////////////////////////////
+    _formatoCorrecto(pdfTexto){
+        let kid = pdfTexto.split('/Kids');//por ahora solo funciona con un Kids
+        if(kid.length==2)
+            return true
+        else 
+            return false
+    }
+    _getPagesObj(pdfTexto){
+
+        let arrayTemp=pdfTexto.split('/Kids')
+        arrayTemp=arrayTemp[1]
+        arrayTemp.tr
+   
+        let rect = arrayTemp.slice(arrayTemp.indexOf("[")+1,arrayTemp.indexOf("]")).trim().split(" ");
+        let pages=[];
+        
+        for(let i=0;i<rect.length;i++){
+            if(i%3==0){
+                pages.push(rect[i])
+            }
+        }
+        return pages;//el primer elemento del array es la primera pagina
+    }
+}
