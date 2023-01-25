34265847a49a52a4c4924a9e86aa95710ce7feb6
Jorge Cotrado <jumanor@gmail.com>
2023-01-24 21:27:43 -0500

    agregamos un simple spin al ejemplo03

---
+++ example/example03/utils/spin.js
@@ -0,0 +1,12 @@
+export class Spin{
+
+    static load(){
+        var modal = document.getElementById("myModalSpin");
+        modal.style.display = "block";
+    }
+
+    static unload(){
+        var modal = document.getElementById("myModalSpin");
+        modal.style.display = "none";
+    }
+}
